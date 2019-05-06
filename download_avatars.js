4b2fa3ffac3474b6669793a26bfcf21c9e5acd85
Kayley <kayley.luftig@gmail.com>
2019-05-06 22:42:33 +0000

    implementing shell for requestand using call back to invoke the function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,12 @@
 var request = require('request');
 
 console.log('Welcome to GitHub Avatar Downloader!');
+
+function getRepoContributors(reboOwner, repoName, cb) {
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
