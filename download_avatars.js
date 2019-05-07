e911a5b6bffb47b79b9a3dcc3bcdf0c4ee5590fb
Kayley <kayley.luftig@gmail.com>
2019-05-07 02:35:34 +0000

    put in error handling incase user does not pass in two arguments

--- download_avatars.js
+++ download_avatars.js
@@ -5,39 +5,42 @@ var fs = require("fs");
 var args = process.argv.slice(2);
 
 console.log("Welcome to GitHub Avatar Downloader!");
+//set error handling if user doesn't specify both arguments
+if (!args[0]) {
+  console.log("ERROR!!! Please enter a username and repo URL");
+} else {
+  // implents request to fetch list of contributors for the given repo
+  function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        "User-Agent": "request",
+        "Authorization": "token" + gitHubToken
+      }
+    };
+    // get page information
+    request(options, function(err, res, body){
+      cb(err, body);
+    });
+  }
 
-// implents request to fetch list of contributors for the given repo
-function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      "User-Agent": "request",
-      "Authorization": "token" + gitHubToken
+  // get request to download images
+  function downloadImageByURL(url, filePath) {
+    request.get(url)
+    .pipe(fs.createWriteStream(filePath));
+  }
+
+  // invokes function above using callback
+  getRepoContributors(args[0], args[1], function(err, result) {
+    console.log("Errors:", err);
+    var data = JSON.parse(result);
+    //loops through array and retrieves avatar link
+    for (var i = 0; i < data.length; i++) {
+      console.log(data[i].avatar_url);
+      downloadImageByURL(data[i].avatar_url, "avatars/" + data[i].login + ".jpg");
     }
-  };
-  request(options, function(err, res, body){
-    cb(err, body);
   });
 }
-
-// get request to download images
-function downloadImageByURL(url, filePath) {
-  request.get(url)
-  .pipe(fs.createWriteStream(filePath));
-}
-
-// invokes function above using callback
-getRepoContributors(args[0], args[1], function(err, result) {
-  console.log("Errors:", err);
-  var data = JSON.parse(result);
-  console.log(data);
-  //loops through array and retrieves avatar link
-  for (var i = 0; i < data.length; i++) {
-    console.log(data[i].avatar_url);
-    downloadImageByURL(data[i].avatar_url, "avatars/" + data[i].login + ".jpg");
-  }
-});
-
 
 
 
