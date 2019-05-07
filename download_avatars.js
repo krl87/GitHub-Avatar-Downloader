27ce35b6aa9c4db4e20e08a129adf7b062a8e9f1
Kayley <kayley.luftig@gmail.com>
2019-05-07 01:49:54 +0000

    downloading images with pipe,request method

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require("request");
 var gitHubToken = require("./secrets.js");
+var fs = require("fs");
 
 console.log("Welcome to GitHub Avatar Downloader!");
 
@@ -12,21 +13,29 @@ function getRepoContributors(repoOwner, repoName, cb) {
       "Authorization": "token" + gitHubToken
     }
   };
-
   request(options, function(err, res, body){
     cb(err, body);
   });
+}
+
+// get request to download images
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+  .pipe(fs.createWriteStream(filePath));
 }
 
 // invokes function above using callback
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   var data = JSON.parse(result);
-  // console.log(data);
   for (var i = 0; i < data.length; i++) {
     console.log(data[i].avatar_url);
   }
 });
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
+
+
 
 
 
